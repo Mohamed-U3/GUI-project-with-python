646711c821346d0b153a0c87a22b7bb79748af35
Mohamed Yousry <engineer.mo.u3@gmail.com>
2022-09-20 11:07:52 +0200

    >> now the program able to select arduino uno automatically >> The program leaves the selection of the port to the user in case not finding port named arduino uno >> The program now has ability of sending Strings to arduino and the arduino can take decision about it

--- main.py
+++ main.py
@@ -6,25 +6,44 @@ while True:
 
     portList = []
 
-    # passing through all the ports that is connected to the computer then printing them
+    # passing through all the ports that is connected to the computer
     for port in ports:
         portList.append(str(port))
-        print(str(port))
-    #input selected port
-    val = input("select port: COM")
-    print("\n")
-    #insure you selected one of the existed port then printing the selected one
+    # automatic Select Arduino if found
     for x in range(0,len(portList)):
-        if portList[x].startswith("COM" + str(val)):
-            portvar = "COM" + str(val)
-            print("\nYou selected")
+        if "Arduino Uno" in portList[x]:
+            portvar = portList[x].split(' ', 1)[0]
+            print("\nComputer selected")
             print(portList[x])
+    
+    # check if Computer Select port or not
+    try:
+        portvar
+    except NameError: # If not selected
+        print("Computer did not select port\n")
+        print("The available Ports are :")
+        # passing through all the ports that is connected to the computer then printing them
+        for port in portList:
+            print(port)
+        #input selected port
+        val = input("\nselect port: COM")
+        #insure you selected one of the existed port then printing the selected one
+        for x in range(0,len(portList)):
+            if portList[x].startswith("COM" + str(val)):
+                portvar = "COM" + str(val)
+                print("\nYou selected")
+                print(portList[x])
+    else:   #If Selected Then initializing serial port
+        serialInst.baudrate = 9600
+        serialInst.port = portvar
+        serialInst.open()
+        break
 
+    # insure user Selected the port manually
     try:
         portvar
     except NameError:
-        print("You selected wrong Port it Seems")
-        print("The available Ports are :")
+        print("You selected non existing Port it Seems")
     else:   #initializing serial port
         serialInst.baudrate = 9600
         serialInst.port = portvar
@@ -32,6 +51,11 @@ while True:
         break
 
 while True:
-    if serialInst.in_waiting:
-        packet = serialInst.readline()
-        print(packet.decode("utf").rstrip('\n'))
+    d = input("Enter the Value: ")
+    if(d == 'H'):
+        serialInst.write(d.encode())
+    elif(d == 'L'):
+        serialInst.write(d.encode())
+    # if serialInst.in_waiting:
+    #     packet = serialInst.readline()
+    #     print(packet.decode("utf").rstrip('\n'))
